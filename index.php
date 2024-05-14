1ff12cd0a440aa599073a99262cb519ab196c2a8
Noel Pérez Villanueva <noelp2317@gmail.com>
2024-05-14 19:59:31 +0200

    Actualizados algunas ayudan pero no quedan visibles

--- index.php
+++ index.php
@@ -10,22 +10,26 @@ require_once('config/const.php');
     <title>Videojuegos</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php echo BOOTSTRAP; ?>css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo JS; ?>bootstrap-notify/bootstrap-notify.min.js">
+    <!-- Bootstrap Notify JavaScript -->
+    <script src="<?php echo JS; ?>bootstrap-notify/bootstrap-notify.min.js"></script>
+    <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Custom CSS -->
     <link rel="stylesheet" href="<?php echo CSS; ?>passwords.css">
 </head>
 <body>
 
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Videojuegos</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
-                    <?php if(isset($_SESSION['user_id'])): ?>
+                    <?php if (isset($_SESSION['user_id'])): ?>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 <?php echo $_SESSION['nick']; ?>
@@ -37,7 +41,8 @@ require_once('config/const.php');
                         </li>
                     <?php else: ?>
                         <li class="nav-item">
-                            <button type="button" class="btn btn-link nav-link" data-bs-toggle="modal" data-bs-target="#loginModal">Iniciar Sesión</button>
+                            <button type="button" class="btn btn-link nav-link" data-bs-toggle="modal" data-bs-target="#loginModal">Iniciar Sesión
+                            </button>
                         </li>
                     <?php endif; ?>
                 </ul>
@@ -51,28 +56,31 @@ require_once('config/const.php');
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="loginModalLabel">Iniciar Sesión</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
                 </div>
                 <div class="modal-body">
                     <form action="router.php" method="post">
                         <div class="mb-3">
                             <label for="loginNick" class="form-label">Nick</label>
-                            <input type="text" class="form-control" id="loginNick" name="nick" required>
+                            <input type="text" class="form-control" id="loginNick" name="nick" required aria-label="Nombre de usuario">
+                            <div id="loginNickHelp" class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="loginPassword" class="form-label">Contraseña</label>
                             <div class="input-group">
-                                <input type="password" class="form-control" id="loginPassword" name="password" required>
-                                <button class= type="button"class="password-toggle-icon">
+                                <input type="password" class="form-control" id="loginPassword" name="password" required aria-label="Contraseña" onpaste="return false">
+                                <button class="btn btn-outline-secondary password-toggle-icon" type="button" id="loginPasswordToggle" aria-label="Mostrar contraseña">
                                     <i class="fa fa-eye"></i>
                                 </button>
                             </div>
+                            <div id="loginPasswordHelp" class="form-text"></div>
                         </div>
                         <input type="hidden" name="action" value="login">
                         <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
                     </form>
                     <div class="text-center mt-3">
-                        <p>¿No tienes cuenta? <a href="#" data-bs-toggle="modal" data-bs-target="#registerModal">Regístrate aquí</a></p>
+                        <p>¿No tienes cuenta? <a href="#" data-bs-toggle="modal" data-bs-target="#registerModal">Regístrate aquí</a>
+                        </p>
                     </div>
                 </div>
             </div>
@@ -85,63 +93,65 @@ require_once('config/const.php');
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="registerModalLabel">Registrarse</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
                 </div>
                 <div class="modal-body">
                     <form action="router.php" method="post">
                         <div class="mb-3">
                             <label for="registerName" class="form-label">Nombre</label>
-                            <input type="text" class="form-control" id="registerName" name="name" required>
+                            <input type="text" class="form-control" id="registerName" name="name" required aria-label="Nombre">
+                            <div id="registerNameHelp" class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="registerNick" class="form-label">Nick</label>
-                            <input type="text" class="form-control" id="registerNick" name="nick" required>
+                            <input type="text" class="form-control" id="registerNick" name="nick" required aria-label="Nombre de usuario">
+                            <div id="registerNickHelp" class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="registerDob" class="form-label">Fecha de Nacimiento</label>
-                            <input type="date" class="form-control" id="registerDob" name="dob" required>
+                            <input type="date" class="form-control" id="registerDob" name="dob" required aria-label="Fecha de nacimiento">
+                            <div id="registerDobHelp" class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="registerEmail" class="form-label">Correo electrónico</label>
-                            <input type="email" class="form-control" id="registerEmail" name="email" required>
+                            <input type="email" class="form-control" id="registerEmail" name="email" required aria-label="Correo electrónico">
+                            <div id="registerEmailHelp" class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="registerPassword" class="form-label">Contraseña</label>
                             <div class="input-group">
-                                <input type="password" class="form-control" id="registerPassword" name="registerPassword" required>
-                                <button type="button" class="password-toggle-icon">
+                                <input type="password" class="form-control" id="registerPassword" name="registerPassword" required aria-label="Contraseña" onpaste="return false">
+                                <button class="btn btn-outline-secondary password-toggle-icon" type="button" id="registerPasswordToggle" aria-label="Mostrar contraseña">
                                     <i class="fa fa-eye"></i>
                                 </button>
                             </div>
-                            <div class="password-strength">
+                            <div class="password-strength mb-3">
                                 <div class="strength-bar"></div>
                                 <div class="strength-text"></div>
                             </div>
-                            <div id="passwordValidity">
-                                <span class="hint">Debe contener al menos 8 caracteres</span>
-                                <span class="hint">Debe contener al menos un número</span>
-                                <span class="hint">Debe contener al menos una letra mayúscula</span>
-                                <span class="hint">Debe contener al menos un carácter especial</span>
+                            <div class="pswd_info">
+                                <div class="notify error">La contraseña debe cumplir los siguientes requisitos:</div>
+                                <ul>
+                                    <li id="letter" class="invalid"><i class="fas fa-times"></i>Al menos <strong>una letra</strong></li>
+                                    <li id="capital" class="invalid"><i class="fas fa-times"></i>Al menos <strong>una letra mayúscula</strong></li>
+                                    <li id="number" class="invalid"><i class="fas fa-times"></i>Al menos <strong>un número</strong></li>
+                                    <li id="length" class="invalid"><i class="fas fa-times"></i>Al menos <strong>8 caracteres</strong></li>
+                                    <li id="null" class="invalid"><i class="fas fa-times"></i>Debe <strong>confirmar la contraseña</strong></li>
+                                    <li id="match" class="invalid"><i class="fas fa-times"></i>Las contraseñas <strong>deben coincidir</strong></li>
+                                    <li id="blank" class="invalid"><i class="fas fa-times"></i>Las contraseñas <strong>no deben tener espacios</strong></li>
+                                </ul>
                             </div>
+                            <div id="registerPasswordHelp" class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="confirmPassword" class="form-label">Confirmar contraseña</label>
                             <div class="input-group">
-                                <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required>
-                                <button type="button" class="password-toggle-icon">
+                                <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" required aria-label="Confirmar contraseña" onpaste="return false">
+                                <button class="btn btn-outline-secondary password-toggle-icon" type="button" id="confirmPasswordToggle" aria-label="Mostrar contraseña">
                                     <i class="fa fa-eye"></i>
                                 </button>
                             </div>
-                            <div class="password-strength">
-                                <div class="strength-bar"></div>
-                                <div class="strength-text"></div>
-                            </div>
-                            <div id="passwordValidity">
-                                <span class="hint">Debe contener al menos 8 caracteres</span>
-                                <span class="hint">Debe contener al menos un número</span>
-                                <span class="hint">Debe contener al menos una letra mayúscula</span>
-                                <span class="hint">Debe contener al menos un carácter especial</span>
-                            </div>
+                            <div id="confirmPasswordHelp" class="form-text"></div>
                         </div>
                         <input type="hidden" name="action" value="register">
                         <button type="submit" class="btn btn-primary">Registrarse</button>
@@ -157,7 +167,6 @@ require_once('config/const.php');
     <!-- Bootstrap Bundle with Popper -->
     <script src="<?php echo BOOTSTRAP; ?>js/bootstrap.bundle.min.js"></script>
     <script src="<?php echo JS; ?>jquery/jquery-3.7.1.min.js"></script>
-    <script src="<?php echo JS; ?>bootstrap-notify/bootstrap-notify.min.js"></script>
     <script src="<?php echo JS; ?>notifications.js"></script>
     <script src="<?php echo JS; ?>passwords.js"></script>
 </body>
--- resources/js/passwords.js
+++ resources/js/passwords.js
@@ -1,36 +1,120 @@
 document.addEventListener('DOMContentLoaded', function() {
     const passwordInputs = document.querySelectorAll('.form-control[type="password"]');
-    const eyeIcons = document.querySelectorAll('.password-toggle-icon i');
+    const eyeIcons = document.querySelectorAll('.password-toggle-icon');
 
-    eyeIcons.forEach(function(icon) {
+    eyeIcons.forEach(function(icon, index) {
         icon.addEventListener('click', function() {
-            const passwordInput = this.parentElement.querySelector('.form-control');
+            const passwordInput = passwordInputs[index];
             togglePasswordVisibility(passwordInput, this);
         });
     });
 
     passwordInputs.forEach(function(input) {
-        input.addEventListener('input', function() {
-            const password = this.value;
-            const strengthContainer = this.closest('.modal-body').querySelector('.password-strength');
-            const strengthText = strengthContainer.querySelector('.strength-text');
-            const strengthBar = strengthContainer.querySelector('.strength-bar');
+        input.addEventListener('focus', function() {
+            const passwordTooltip = this.querySelector('.pswd_info');
+            if (passwordTooltip) {
+                const tooltipPosition = this.getBoundingClientRect();
+                passwordTooltip.style.top = tooltipPosition.bottom + 'px';
+                passwordTooltip.classList.add('active');
+            }
+        });
 
-            const strength = calculatePasswordStrength(password);
-            updatePasswordStrength(strength, strengthText, strengthBar);
+        input.addEventListener('blur', function() {
+            const passwordTooltip = this.querySelector('.pswd_info');
+            if (passwordTooltip) {
+                passwordTooltip.classList.remove('active');
+            }
+        });
+
+        input.addEventListener('keyup', function() {
+            const password = this.value;
+            const p1 = document.getElementById('registerPassword');
+            const p2 = document.getElementById('confirmPassword');
+            const noValido = /\s/;
+
+            // Validar longitud de contraseña
+            if(password.length<8){
+                document.getElementById('length').classList.remove('valid');
+                document.getElementById('length').classList.add('invalid');
+            } else {
+                document.getElementById('length').classList.remove('invalid');
+                document.getElementById('length').classList.add('valid');
+            }
+
+            // Validar letras
+            if(password.match(/[A-z]/)){
+                document.getElementById('letter').classList.remove('invalid');
+                document.getElementById('letter').classList.add('valid');
+            } else {
+                document.getElementById('letter').classList.remove('valid');
+                document.getElementById('letter').classList.add('invalid');
+            }
+
+            // Validar mayúsculas
+            if(password.match(/[A-Z]/)){
+                document.getElementById('capital').classList.remove('invalid');
+                document.getElementById('capital').classList.add('valid');
+            } else {
+                document.getElementById('capital').classList.remove('valid');
+                document.getElementById('capital').classList.add('invalid');
+            }
+
+            // Validar números
+            if(password.match(/\d/)){
+                document.getElementById('number').classList.remove('invalid');
+                document.getElementById('number').classList.add('valid');
+            } else {
+                document.getElementById('number').classList.remove('valid');
+                document.getElementById('number').classList.add('invalid');
+            }
+
+            // Validar espacios
+            if(noValido.test(p1) || noValido.test(p2)){
+                document.getElementById('blank').classList.remove('valid');
+                document.getElementById('blank').classList.add('invalid');
+            } else {
+                document.getElementById('blank').classList.remove('invalid');
+                document.getElementById('blank').classList.add('valid');
+            }
+
+            // Validar contraseñas iguales
+            if(p1.value!==p2.value){
+                document.getElementById('match').classList.remove('valid');
+                document.getElementById('match').classList.add('invalid');
+            } else {
+                document.getElementById('match').classList.remove('invalid');
+                document.getElementById('match').classList.add('valid');
+            }
         });
     });
+
+    const registerPasswordInput = document.querySelector('#registerPassword');
+    const registerStrengthContainer = document.querySelector('.password-strength');
+
+    if (registerPasswordInput && registerStrengthContainer) {
+        registerPasswordInput.addEventListener('input', function() {
+            const password = this.value;
+            const strengthText = registerStrengthContainer.querySelector('.strength-text');
+            const strengthBar = registerStrengthContainer.querySelector('.strength-bar');
+
+            // Calcular la fortaleza de la contraseña
+            const strength = calculatePasswordStrength(password);
+
+            // Actualizar la barra de fortaleza de la contraseña
+            updatePasswordStrength(strength, strengthText, strengthBar);
+        });
+    }
 });
 
 function togglePasswordVisibility(passwordInput, eyeIcon) {
     if (passwordInput.getAttribute('type') === 'password') {
         passwordInput.setAttribute('type', 'text');
-        eyeIcon.classList.remove('fa-eye');
-        eyeIcon.classList.add('fa-eye-slash');
+        eyeIcon.querySelector('i').classList.remove('fa-eye');
+        eyeIcon.querySelector('i').classList.add('fa-eye-slash');
     } else {
         passwordInput.setAttribute('type', 'password');
-        eyeIcon.classList.remove('fa-eye-slash');
-        eyeIcon.classList.add('fa-eye');
+        eyeIcon.querySelector('i').classList.remove('fa-eye-slash');
+        eyeIcon.querySelector('i').classList.add('fa-eye');
     }
 }
 
@@ -61,38 +145,38 @@ function calculatePasswordStrength(password) {
 }
 
 function updatePasswordStrength(strength, strengthText, strengthBar) {
-    // Actualizar la barra de fuerza
-    strengthBar.style.width = (strength * 20) + '%';
+    if (strengthBar) {
+        strengthBar.style.width = (strength * 20) + '%';
 
-    // Actualizar el texto de la fuerza
-    switch (strength) {
-        case 0:
-        case 1:
-            strengthText.textContent = 'Muy débil';
-            strengthText.style.color = '#d32f2f';
-            strengthBar.style.backgroundColor = '#d32f2f';
-            break;
-        case 2:
-            strengthText.textContent = 'Débil';
-            strengthText.style.color = '#f57c00';
-            strengthBar.style.backgroundColor = '#f57c00';
-            break;
-        case 3:
-            strengthText.textContent = 'Moderada';
-            strengthText.style.color = '#fdd835';
-            strengthBar.style.backgroundColor = '#fdd835';
-            break;
-        case 4:
-            strengthText.textContent = 'Fuerte';
-            strengthText.style.color = '#7cb342';
-            strengthBar.style.backgroundColor = '#7cb342';
-            break;
-        case 5:
-            strengthText.textContent = 'Muy fuerte';
-            strengthText.style.color = '#43a047';
-            strengthBar.style.backgroundColor = '#43a047';
-            break;
-        default:
-            strengthText.textContent = '';
+        switch (strength) {
+            case 0:
+            case 1:
+                strengthText.textContent = 'Muy débil';
+                strengthText.style.color = '#d32f2f';
+                strengthBar.style.backgroundColor = '#d32f2f';
+                break;
+            case 2:
+                strengthText.textContent = 'Débil';
+                strengthText.style.color = '#f57c00';
+                strengthBar.style.backgroundColor = '#f57c00';
+                break;
+            case 3:
+                strengthText.textContent = 'Moderada';
+                strengthText.style.color = '#fdd835';
+                strengthBar.style.backgroundColor = '#fdd835';
+                break;
+            case 4:
+                strengthText.textContent = 'Fuerte';
+                strengthText.style.color = '#7cb342';
+                strengthBar.style.backgroundColor = '#7cb342';
+                break;
+            case 5:
+                strengthText.textContent = 'Muy fuerte';
+                strengthText.style.color = '#43a047';
+                strengthBar.style.backgroundColor = '#43a047';
+                break;
+            default:
+                strengthText.textContent = '';
+        }
     }
 }
