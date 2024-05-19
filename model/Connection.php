750a2497a486de87d8f1cf149bfe634135fbcd67
Noel Pérez Villanueva <noelp2317@gmail.com>
2024-05-19 20:43:25 +0200

    Se modifica la documentación de la clase Connection.php y se agrega el método close() para cerrar la conexión a la base de datos.

--- model/Connection.php
+++ model/Connection.php
@@ -4,11 +4,23 @@ require_once(CONFIG.'Database.php');
 // Clase para manejar la conexión con la base de datos específicamente
 class Connection extends Database
 {
-    // Constructor de la clase
+    /**
+     * Constructor de la clase
+     * Llama al constructor de la clase padre con los parámetros específicos
+     */
     public function __construct()
     {
         // Llamar al constructor de la clase padre
         parent::__construct("localhost", "gam3Coll@bUs3r", "J4nR#9!pQz_23Ld", "utf8mb4","videogame_collection");
+    }
+
+    /**
+     * Método para establecer la conexión con la base de datos
+     * Llama al método connect de la clase padre
+     */
+    public function close()
+    {
+        $this->conn = null;
     }
 }
 ?>
