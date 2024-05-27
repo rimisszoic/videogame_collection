f9236ecc3473f64fb369462bb3f583a4d95af314
Noel Pérez Villanueva <noelp2317@gmail.com>
2024-05-27 13:45:46 +0200

    Script para cambiar los permisos

---
+++ changue-permisons.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Cambiar permisos a las carpetas
+chmod 0755 ./config
+chmod 0755 ./controller
+chmod 0755 ./logs
+chmod 0755 ./model
+chmod 0755 ./resources
+chmod 0755 ./config/*.php
+chmod 0755 ./controller/*.php
+chmod 0755 ./model/*.php
+chmod 0755 -R ./resources/
